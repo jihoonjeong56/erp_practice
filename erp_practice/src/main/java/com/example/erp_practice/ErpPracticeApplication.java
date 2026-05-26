8ec4fc6f957d62f481e1d79e3b305ae8512e2cf3
jihoonjeong56 <jeong283056@gmail.com>
2026-05-26 16:32:02 +0900

    add erp base with springboot

---
+++ erp_practice/src/main/java/com/example/erp_practice/ErpPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.erp_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpPracticeApplication.class, args);
+	}
+
+}

7b4d887d1fbf9bc254a6b992b6840473c8c6f3d9
jihoonjeong56 <jeong283056@gmail.com>
2026-07-07 18:27:27 +0900

    feature CommonCodeController ~ing

--- erp/src/main/java/com/example/erp/domain/code/controller/CommonCodeController.java
+++ erp/src/main/java/com/example/erp/domain/code/controller/CommonCodeController.java
@@ -1,4 +1,54 @@
 package com.example.erp.domain.code.controller;
 
+import com.example.erp.common.response.ApiResponse;
+import com.example.erp.domain.code.dto.CodeGroupCreateRequest;
+import com.example.erp.domain.code.dto.CodeGroupResponse;
+import com.example.erp.domain.code.dto.CommonCodeCreateRequest;
+import com.example.erp.domain.code.service.CommonCodeService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/codes")
+@RequiredArgsConstructor
 public class CommonCodeController {
+    private final CommonCodeService commonCodeService;
+
+    @PostMapping("/groups")
+    public ResponseEntity<ApiResponse<String>> createGroup(
+            @Valid @RequestBody CodeGroupCreateRequest request
+            ){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.ok(commonCodeService.createGroup(request)));
+    }
+
+    @GetMapping("/groups")
+    public ResponseEntity<ApiResponse<List<CodeGroupResponse>>> getAllGroups(){
+        return ResponseEntity.ok(ApiResponse.ok(commonCodeService.getAllGroups()));
+    }
+
+    @GetMapping("/groups/{groupCode}")
+    public ResponseEntity<ApiResponse<CodeGroupResponse>> getGroup(
+            @PathVariable String groupCode
+    ){
+        return ResponseEntity.ok(ApiResponse.ok(commonCodeService.getGroup(groupCode)));
+    }
+
+//    ----------------------------------------------------------------------------------
+    
+    @PostMapping("/groups/{groupCode}/codes")
+    public ResponseEntity<ApiResponse<Long>> createCode(
+            @PathVariable String groupCode,
+            @Valid @RequestBody CommonCodeCreateRequest request
+            ){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.ok(commonCodeService.createCode(groupCode, request)));
+    }
+
+
 }

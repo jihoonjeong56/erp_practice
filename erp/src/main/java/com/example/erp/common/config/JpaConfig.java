be63d2d2d18fbaf583821abc7f6460e1f6cb7b24
jihoonjeong56 <jeong283056@gmail.com>
2026-07-08 18:52:09 +0900

    update JpaConfig for Caching

--- erp/src/main/java/com/example/erp/common/config/JpaConfig.java
+++ erp/src/main/java/com/example/erp/common/config/JpaConfig.java
@@ -1,9 +1,11 @@
 package com.example.erp.common.config;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
+@EnableCaching
 public class JpaConfig {
 }

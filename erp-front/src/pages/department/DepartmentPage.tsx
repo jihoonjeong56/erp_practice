eb31d48730191b6a452c5460391074292a827e22
jihoonjeong56 <jeong283056@gmail.com>
2026-07-16 18:43:11 +0900

    feature DepartmentPage ~ing

--- erp-front/src/pages/department/DepartmentPage.tsx
+++ erp-front/src/pages/department/DepartmentPage.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import type { Department } from "../../types/department";
+import { getDepartments } from "../../api/department.api";
 
 export default function DepartmentPage() {
-    
-  return (
-    <div>DepartmentPage</div>
-  )
+  const [departments, setDepartments] = useState<Department[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [showModel, setShowModel] = useState(false);
+  const [editTarget, setEditTarget] = useState<Department | null>(null);
+  const [error, setError] = useState("");
+
+  const initialForm = {
+    deptCode: "",
+    deptName: "",
+    description: "",
+    sortOrder: 0,
+    parentId: null as number | null,
+    useYn: "Y",
+  };
+  const [form, setForm] = useState(initialForm);
+
+  // 조회
+  const fetchDepartments = async () => {
+    setLoading(true);
+    try {
+      const data = await getDepartments();
+      setDepartments(data);
+    } catch {
+      setError("부서목록을 불러오지 못했습니다.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDepartments();
+  }, []);
+  return <div>DepartmentPage</div>;
 }

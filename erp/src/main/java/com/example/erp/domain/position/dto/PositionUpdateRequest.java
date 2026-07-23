4ccf1214ee5367f18620795e303bd220fdc8eec4
jihoonjeong56 <jeong283056@gmail.com>
2026-07-23 17:41:16 +0900

    feature Position Create Update Request

--- erp/src/main/java/com/example/erp/domain/position/dto/PositionUpdateRequest.java
+++ erp/src/main/java/com/example/erp/domain/position/dto/PositionUpdateRequest.java
@@ -4,14 +4,13 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
-public class PositionCreateRequest {
-
-    @NotBlank(message = "직급코드는 필수입니다.")
-    private String posCode;
-
+public class PositionUpdateRequest {
     @NotBlank(message = "직급명은 필수입니다.")
-    private String posNAme;
+    private String posName;
 
     @NotBlank(message = "직급레벨은 필수입니다.")
     private int level;
+
+    @NotBlank(message = "사용여부는 필수 입니다.")
+    private String useYn;
 }

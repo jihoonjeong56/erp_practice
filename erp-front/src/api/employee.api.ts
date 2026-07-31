1dc26ccfc686cc6c28ce5e147ab250510bdfa416
jihoonjeong56 <jeong283056@gmail.com>
2026-07-31 16:13:10 +0900

    feature employee and position api

---
+++ erp-front/src/api/employee.api.ts
@@ -0,0 +1,48 @@
+import { data } from "react-router-dom";
+import type { ApiResponse } from "../types/common";
+import type {
+  Employee,
+  EmployeeCreateRequest,
+  EmployeeUpdateRequest,
+  Position,
+} from "../types/employee";
+import axiosInstance from "./axiosInstance";
+
+export const getEmployees = async (): Promise<Employee[]> => {
+  const res =
+    await axiosInstance.get<ApiResponse<Employee[]>>("/api/employees");
+  return res.data.data;
+};
+
+export const getEmployee = async (id: number): Promise<Employee> => {
+  const res = await axiosInstance.get<ApiResponse<Employee>>(
+    `/api/employees/${id}`,
+  );
+  return res.data.data;
+};
+
+export const createEmployee = async (
+  data: EmployeeCreateRequest,
+): Promise<number> => {
+  const res = await axiosInstance.post<ApiResponse<number>>(
+    "/api/employees",
+    data,
+  );
+  return res.data.data;
+};
+
+export const updateEmployee = async (
+  id: number,
+  data: EmployeeUpdateRequest,
+): Promise<void> => {
+  await axiosInstance.put(`/api/employees/${id}`, data);
+};
+
+export const deleteEmployee = async (id: number): Promise<void> => {
+  await axiosInstance.delete(`/api/employees/${id}`);
+};
+
+export const getPositions = async (): Promise<Position[]> => {
+  const res = await axiosInstance.get<ApiResponse<Position[]>>("api/positions");
+  return res.data.data;
+};
